65d8e5120cc74d1617d6363d068f952ef4b7b078
sinyeong10@gmail.com <sinyeong@torooc.com>
2026-03-29 22:23:25 +0900

    feature(first_init) : 코드 작성 구조 초안

---
+++ src/mazegame/controller.py
@@ -0,0 +1,10 @@
+class MazeView:
+    def __init__(self, view, model):
+        self.view = view
+        self.model = model
+
+    def run(self):
+        pass
+
+    def process_input(self, key):
+        pass
